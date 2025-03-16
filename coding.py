eef5b1e057755486cddd45a46cb164a95e8d6c79
Aman Chaturvedi <62074888+aman774554@users.noreply.github.com>
2025-03-16 21:58:14 +0530

    Update coding.py

--- coding.py
+++ coding.py
@@ -13,3 +13,20 @@ print({k:v for k,v in dict1.items() if k not in "abc"})
 print(sum(dict1.values()))
 ================================================
 # string
+str1 = "naman"
+s1, s2 = "listen", "silent"
+str2 = "aman chaturvedi"
+
+print(str1[::-1])
+print(str1 == str1[::-1])
+print(str1.count("a"))
+print(str1.replace('n','zzz'))
+print(str1.upper())
+print(str1.lower())
+print(sorted(s1)==sorted(s2))
+print(''.join([char for char in str1 if char.lower() not in  'aeiou']))
+print(len(s1) == len(s2) and s2 in s1) 
+print(' '.join((str2.split(" ")[::-1])))
+================================================
+#List
+
